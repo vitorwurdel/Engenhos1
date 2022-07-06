f2af9ad461895e5e9ab5a4f1514fa5779790cdcf
vitorwurdel <vitor.wurdel@sicred.com>
2022-07-05 23:04:08 -0300

    Adicionar arquivos de projeto.

---
+++ Engenhos1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Engenhos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, soma = 0;
+
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++) {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine("A soma dos números multiplos de 3 e 5 entre 0 e " + numero + " é: " + soma);
+        }
+    }
+}
